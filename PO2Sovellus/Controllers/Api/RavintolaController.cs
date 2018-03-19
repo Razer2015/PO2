f0f2845956f50111ba14776eceb43f4899164666
Razer2015 <razer138@gmail.com>
2018-03-19 14:17:09 +0200

    - Harjoitus 9 kesken

---
+++ PO2Sovellus/Controllers/Api/RavintolaController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PO2Sovellus.ViewModels;
+using Sovellus.Data.Repositories;
+using Sovellus.Model.Entities;
+using System;
+
+namespace PO2Sovellus.Controllers.Api
+{
+    [Route("api/ravintola")]
+    public class RavintolaController : Controller
+    {
+        private IRavintolaRepository _ravintolaData;
+
+        public RavintolaController(IRavintolaRepository ravintolaData) {
+            _ravintolaData = ravintolaData;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id) {
+            Ravintola malli = _ravintolaData.Hae(id);
+            if (malli == null) {
+                return BadRequest("Ravintolaa ei löytynyt.");
+            }
+
+            return Ok(malli);
+        }
+
+        [HttpPost("")]
+        public IActionResult Post([FromBody]RavintolaApiViewModel malli) {
+            if (ModelState.IsValid) {
+                Mapper.Initialize(config => { config.CreateMap<RavintolaApiViewModel, Ravintola>(); });
+                Ravintola uusi = Mapper.Map<Ravintola>(malli);
+
+                uusi = _ravintolaData.Lisaa(uusi);
+                if (uusi != null) {
+                    return Created($"api/ravintola/{uusi.Id}", uusi);
+                }
+            }
+            return BadRequest(ModelState);
+            //return BadRequest("Ravintolaa ei voitu lisätä.");
+        }
+
+        [HttpPut("")]
+        public IActionResult Put([FromBody]RavintolaApiViewModel malli) {
+            if (ModelState.IsValid) {
+                Mapper.Initialize(config => {
+                    config.CreateMap<RavintolaApiViewModel, Ravintola>();
+                });
+                Ravintola muutettava = Mapper.Map<Ravintola>(malli);
+                muutettava = _ravintolaData.Muuta(muutettava);
+                if (muutettava != null) {
+                    return Ok(muutettava);
+                }
+            }
+            return BadRequest(ModelState);
+            //return BadRequest("Ravintolaa ei voitu muuttaa.");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) {
+            Ravintola poistettava = _ravintolaData.Hae(id);
+            if (poistettava != null) {
+                if (_ravintolaData.Poista(poistettava)) {
+                    return Ok();
+                }
+            }
+            return BadRequest("Ravintolaa ei voitu poistaa.");
+        }
+    }
+}
---
+++ PO2Sovellus/ViewModels/RavintolaApiViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PO2Sovellus.ViewModels
+{
+    public class RavintolaApiViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nimi on pakollinen"),
+        MaxLength(200, ErrorMessage = "Nimi voi olla korkeintaan 200 merkkiä pitkä.")]
+        public string Nimi { get; set; }
+        [Required(ErrorMessage = "Kaupunki on pakollinen")]
+        public int KaupunkiId { get; set; }
+        public int? TyyppiId { get; set; }
+        public string Katuosoite { get; set; }
+        [StringLength(5, MinimumLength = 5,
+        ErrorMessage = "Postinumero on oltava tasan 5 merkkiä.")]
+        public string Postinro { get; set; }
+        public string KuvaUrl { get; set; }
+        public string KotisivuUrl { get; set; }
+    }
+}
